8327f3b177da7eb66994131468dc97199ecdfc33
Anton Gushcha <ncrashed@protonmail.com>
2022-03-03 21:40:04 +0300

    Save frames to files

--- zercalo-render/src/main.rs
+++ zercalo-render/src/main.rs
@@ -1,12 +1,15 @@
+use fast_voxel_traversal::raycast_3d::*;
+use glam::f32::Quat;
+use glam::{IVec3, Vec3};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
+use sdl2::pixels::{Color, PixelFormatEnum};
 use sdl2::rect::{Point, Rect};
 use sdl2::render::{Canvas, Texture, TextureCreator};
 use sdl2::video::{Window, WindowContext};
-use fast_voxel_traversal::raycast_3d::*;
-use glam::{Vec3, IVec3};
-use glam::f32::Quat;
+use std::fs::File;
+use std::io::BufWriter;
+use std::path::Path;
 
 const TILE_SIZE: u32 = 64;
 const WINDOW_WIDTH: u32 = 1024;
@@ -15,15 +18,39 @@ const PIXEL_SIZE: f32 = 0.7;
 const FRAMES_COUNT: u32 = 128;
 const VOLUME_SIZE: u32 = 16;
 
-fn render_frame<'a>(
+fn save_png(str_path: &str, data: &[u8], width: u32, height: u32) {
+    let path = Path::new(str_path);
+    let file = File::create(path).unwrap();
+    let w = BufWriter::new(file);
+
+    let mut encoder = png::Encoder::new(w, width, height);
+    encoder.set_color(png::ColorType::Rgba);
+    encoder.set_depth(png::BitDepth::Eight);
+    encoder.set_trns(vec![0xFFu8, 0xFFu8, 0xFFu8, 0xFFu8]);
+    encoder.set_source_gamma(png::ScaledFloat::from_scaled(45455)); // 1.0 / 2.2, scaled by 100000
+    encoder.set_source_gamma(png::ScaledFloat::new(1.0 / 2.2)); // 1.0 / 2.2, unscaled, but rounded
+    let source_chromaticities = png::SourceChromaticities::new(
+        // Using unscaled instantiation here
+        (0.31270, 0.32900),
+        (0.64000, 0.33000),
+        (0.30000, 0.60000),
+        (0.15000, 0.06000),
+    );
+    encoder.set_source_chromaticities(source_chromaticities);
+    let mut writer = encoder.write_header().unwrap();
+
+    writer.write_image_data(data).unwrap();
+}
+
+fn render_frames<'a>(
     canvas: &mut Canvas<Window>,
     texture_creator: &'a TextureCreator<WindowContext>,
 ) -> Result<Vec<Texture<'a>>, String> {
     let mut frames = vec![];
-    for i in 0..FRAMES_COUNT {
+    for _ in 0..FRAMES_COUNT {
         let frame = texture_creator
-        .create_texture_target(None, TILE_SIZE, TILE_SIZE)
-        .map_err(|e| e.to_string())?;
+            .create_texture_target(None, TILE_SIZE, TILE_SIZE)
+            .map_err(|e| e.to_string())?;
         frames.push(frame);
     }
 
@@ -46,14 +73,14 @@ fn render_frame<'a>(
 
                 for i in 0..TILE_SIZE {
                     for j in 0..TILE_SIZE {
-
                         let quat = Quat::from_axis_angle(Vec3::Y, angle);
                         let eye = quat.mul_vec3(Vec3::new(32.0, 32.0, 32.0));
                         let halfsize = VOLUME_SIZE as f32 * 0.5;
                         let camdir = (Vec3::new(halfsize, halfsize, halfsize) - eye).normalize();
                         let up = Vec3::new(0.0, 1.0, 0.0);
                         let right = camdir.cross(up);
-                        let offset = up * ((j as f32 - 0.5 * TILE_SIZE as f32) * PIXEL_SIZE) + right * ((i as f32 - 0.5 * TILE_SIZE as f32) * PIXEL_SIZE);
+                        let offset = up * ((j as f32 - 0.5 * TILE_SIZE as f32) * PIXEL_SIZE)
+                            + right * ((i as f32 - 0.5 * TILE_SIZE as f32) * PIXEL_SIZE);
 
                         let ray = Ray3 {
                             origin: (eye + offset).into(),
@@ -82,11 +109,25 @@ fn render_frame<'a>(
                             texture_canvas
                                 .draw_point(Point::new(i as i32, (TILE_SIZE - j) as i32))
                                 .expect("could not draw point");
-
-
                         }
                     }
                 }
+            })
+            .map_err(|e| e.to_string())?;
+
+        canvas
+            .with_multiple_texture_canvas(textures.iter(), |texture_canvas, ctx| {
+                let (i, _) = *ctx;
+                println!("Saving frame {}/{}", i, FRAMES_COUNT);
+                let pixels = texture_canvas
+                    .read_pixels(None, PixelFormatEnum::ARGB8888)
+                    .expect("Cannot read pixels from frame");
+                save_png(
+                    &format!("frame_{:0>4}.png", i),
+                    &pixels,
+                    TILE_SIZE,
+                    TILE_SIZE,
+                );
             })
             .map_err(|e| e.to_string())?;
     }
@@ -117,10 +158,10 @@ fn main() -> Result<(), String> {
     canvas.clear();
     canvas.present();
 
-    let texture_creator: TextureCreator<_> = canvas.texture_creator();
-    let frames = render_frame(&mut canvas, &texture_creator)?;
-
     let mut event_pump = sdl_context.event_pump()?;
+    let texture_creator: TextureCreator<_> = canvas.texture_creator();
+    let frames = render_frames(&mut canvas, &texture_creator)?;
+
     let mut counter: u32 = 0;
     let mut frame = 0;
     'running: loop {
